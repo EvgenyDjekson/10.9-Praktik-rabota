15e85c91db2424a5c89d9a542441329f1ce80561
EvgenyDjekson <kunz-evgeny@yandex.ru>
2025-03-24 22:57:59 +0300

    дискриминани  больше 0

--- 10.9 Практическая работа.cpp
+++ 10.9 Практическая работа.cpp
@@ -8,20 +8,19 @@ int main()
     float a, b, c;
     std::cout << "a, b, c:";
     std::cin >> a >> b >> c;
-    if (a < 0)
+    
+    float discriminant = (b * b) - 4 * a * c;
+
+    if (discriminant < 0)
     {
-        std::cout << "Not a quadratic equation!" << std::endl;
+        std::cout << "There is no solution!" << std::endl;
     }
-    else if (b > 0)
-    {
-        float discriminant = (b * b) - 4 * a * c;
-        
-        if (discriminant > 1)
-        {
-            float x1 = (-b + std::sqrt(discriminand)) / (2 * a);
-            float x2 = (-b + std::sqrt(discriminand)) / (2 * a),
-                std::cout << "Root 1, 2: " << x1 << ", " << x1 << std::endl;
-        }
+    else if (discriminant  > 0) {
+
+       float x1 = (-b + std::sqrt(discriminant)) / (2 * a);
+       float x2 = (-b - std::sqrt(discriminant)) / (2 * a);
+       std::cout << "Root 1, 2: " << x1 << ", " << x1 << std::endl;
+    }
         else if (discriminant == 0)
         {
             float x = b + std::sqrt(discriminant * discriminant) / (2 * a);
@@ -31,6 +30,6 @@ int main()
         {
             std::cout << "Complex scenario is not supported!" << std::endl;
         }
-    }
+    
     return 0;
 }
